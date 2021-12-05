f7f1a1b4cb55253bc62b0b5139b522c6b13917be
Ana Beatriz <anabeatrizkapps@ice.ufjf.br>
2021-12-05 19:43:07 -0300

    alteração echo

--- client.py
+++ client.py
@@ -12,18 +12,21 @@ def main():
   client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   client.connect(ADDR)
   print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
+  
 
   connected = True
   while connected:
     msg = input("> ")
 
-    client.send(msg.encode(FORMAT))
+    # client.send(msg.encode(FORMAT))
 
-    if msg == QUIT_MSG:
-      connected = False
-    else:
+    if 'echo' in msg or 'ECHO' in msg:
+      client.send(msg.encode(FORMAT))
       msg = client.recv(SIZE).decode(FORMAT)
       print(f"[SERVER] {msg}")
+    elif(QUIT_MSG in msg):
+      client.send(msg.encode(FORMAT))
+      connected = False
 
 if __name__ == "__main__":
   main()
